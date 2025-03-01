cfead6193c141a80a813622e871d221d6b9bcd46
Rupam <rupamsaha8100@gamil.com>
2025-03-01 13:58:52 +0530

    Gemini ChatBot

---
+++ Gemini-chat/src/test/java/com/AI/Gemini_chat/GeminiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI.Gemini_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
